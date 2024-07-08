404d2b8b1368e3feeb279ce4814e7312421ab17e
saviita <taatt2000@gmail.com>
2024-07-09 00:28:29 +0200

    09/07

--- src/js/index.js
+++ src/js/index.js
@@ -7,46 +7,65 @@ const containerTasksElement = document.getElementById("tasks");
 const filterNumberElement = document.getElementById("filter-number");
 const checkboxElement = document.getElementById("checkbox");
 
-let taskCounter = 1;
+const allTasks = [
+  {
+    id: Date.now(),
+    task: "Comprar pan",
+    completed: false,
+  },
+];
 
-const addTaskCounter = () => {
-  // const taskElement = document.querySelectorAll(".task");
-  console.log(taskElement);
+const insertTask = () => {
+  const fragment = document.createDocumentFragment();
 
-  taskCounter++;
-  filterNumberElement.textContent = `${taskCounter} items left`;
+  allTasks.forEach((task) => {
+    const newTask = document.createElement("div");
+    newTask.classList.add("task");
+
+    const newCheckbox = document.createElement("input");
+    newCheckbox.setAttribute("type", "checkbox");
+    newCheckbox.classList.add("task__checkbox");
+
+    const newText = document.createElement("span");
+    newText.textContent = task.task;
+    newText.classList.add("task__text");
+
+    const newImg = document.createElement("img");
+    newImg.src = "./assets/images/icon-cross.svg";
+    newImg.classList.add("task__cross");
+
+    newTask.append(newCheckbox, newText, newImg);
+    fragment.append(newTask);
+  });
+
+  containerTasksElement.append(fragment);
 };
 
-const createTask = (event) => {
+const saveTask = (task) => {
+  allTtasks.push(task);
+
+  insertTask();
+};
+
+const createTask = (task) => {
+  const newTask = [
+    {
+      id: Date.now(),
+      task: task,
+      completed: false,
+    },
+  ];
+
+  saveTask(newTask);
+};
+
+const getTask = (event) => {
   event.preventDefault();
+
+  if (!taskValue) return;
   const taskValue = inputTaskElement.value;
+  createTask(taskValue);
 
-  const newTask = document.createElement("div");
-  newTask.classList.add("task");
-
-  const newCheckbox = document.createElement("input");
-  newCheckbox.setAttribute("type", "checkbox");
-  newCheckbox.classList.add("task__checkbox", "checkbox");
-  newCheckbox.id.add("checkbox");
-
-  const newText = document.createElement("span");
-  newText.textContent = taskValue;
-  newText.classList.add("task__text");
-
-  const newImg = document.createElement("img");
-  newImg.src = "./assets/images/icon-cross.svg";
-  newImg.classList.add("task__cross");
-
-  newTask.append(newCheckbox, newText, newImg);
-  containerTasksElement.append(newTask);
-
-  addTaskCounter();
+  event.target.reset();
 };
-formElement.addEventListener("submit", createTask);
-
-const substractTaskCounter = (event) => {
-  if (taskCounter > 0) {
-    taskCounter--;
-  }
-};
-checkboxElement.addEventListener("input", substractTaskCounter);
+formElement.addEventListener("submit", getTask);
